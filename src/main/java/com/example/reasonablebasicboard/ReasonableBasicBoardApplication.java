d575556965f01a303b8345906b8d597e6909fc0c
fnzl54 <rnjscksdud1@naver.com>
2023-11-27 19:54:25 +0900

    Initial commit

---
+++ src/main/java/com/example/reasonablebasicboard/ReasonableBasicBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.reasonablebasicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasonableBasicBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReasonableBasicBoardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/reasonablebasicboard/ReasonableBasicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reasonablebasicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReasonableBasicBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
